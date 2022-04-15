852b75e453b42bc94c61d0066bb077225f61fe59
chinmaymokashicm <chinmaymokashicm@gmail.com>
2022-04-14 22:19:54 -0500

    Cleaned the code; removed unnecessary functions and variables

--- dashboard/src/components/Patient.js
+++ dashboard/src/components/Patient.js
@@ -5,39 +5,11 @@ import { TextField, Autocomplete, Stack } from "@mui/material";
 import getData from "../functions/getData";
 
 function Patient() {
-  const [patientData, setPatientData] = useState(null);
   const [encounterData, setEncounterData] = useState(null);
   const [timeline, setTimeline] = useState(null);
   const [autoCompleteOptions, setAutoCompleteOptions] = useState([
     "Start typing",
   ]);
-  const [autoCompleteText, setAutoCompleteText] = useState(null);
-
-  async function getPatientIdArray(autoCompleteText) {
-    const patientIds = await getData(
-      null,
-      "patients",
-      {
-        $regex: `.*${autoCompleteText}.*`,
-      },
-      {
-        Id: 1,
-        _id: 0,
-      }
-    );
-    const patientIdArray = patientIds.map((object) => object["Id"]);
-    setAutoCompleteOptions(patientIdArray);
-  }
-
-  async function getEncounterData(patientId) {
-    const encounterDataResults = await getData(null, "encounters", {
-      query: {
-        PATIENT: patientId,
-      },
-    });
-    setEncounterData(encounterDataResults);
-    console.log(encounterDataResults)
-  }
 
   return (
     <div className="patient">
@@ -51,17 +23,34 @@ function Patient() {
                 label="Patient ID"
                 onChange={async (e) => {
                   if ((e.target.value !== "") | (e.target.value !== null)) {
-                    getPatientIdArray(e.target.value);
+                    // getPatientIdArray(e.target.value);
+                    const patientIds = await getData(
+                      null,
+                      "patients",
+                      {
+                        $regex: `.*${e.target.value}.*`,
+                      },
+                      {
+                        Id: 1,
+                        _id: 0,
+                      }
+                    );
+                    const patientIdArray = patientIds.map(
+                      (object) => object["Id"]
+                    );
+                    setAutoCompleteOptions(patientIdArray);
                   }
                 }}
               />
             )}
             clearOnBlur={false}
-            value={autoCompleteText}
-            onChange={(e, patientId) => {
+            onChange={async (e, patientId) => {
               // Get timeline of the selected Id
-              console.log(patientId);
-              getEncounterData(patientId)
+              const encounterDataResults = await getData(e, "encounters", {
+                PATIENT: patientId,
+              });
+              setEncounterData(encounterDataResults);
+              console.log(encounterDataResults);
             }}
           />
         </Stack>
